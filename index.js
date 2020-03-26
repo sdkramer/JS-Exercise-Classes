c465be2329d9e0e453499ec68cfd420fa1e890dc
Sean Kramer <seandominickramer@gmail.com>
2020-03-26 11:56:04 -0400

    Add constructor class Instructor extending Lambdasian

--- index.js
+++ index.js
@@ -133,7 +133,13 @@ class Lambdasian {
         + `demo` receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
         + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
 */
-class Instructor {
+class Instructor extends Lambdasian {
+  constructor(instrAttrs){
+    super(instrAttrs);
+    this.specialty = instrAttrs.specialty;
+    this.favLanguage = instrAttrs.favLanguage;
+    this.catchPhrase = instrAttrs.catchPhrase;
+  }
 
 }
 
