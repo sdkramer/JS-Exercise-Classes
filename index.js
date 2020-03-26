c71855fdc6318565613f07d9e3cc228bfba0802d
Sean Kramer <seandominickramer@gmail.com>
2020-03-26 10:19:28 -0400

    Add answers to test 1

--- index.js
+++ index.js
@@ -41,6 +41,22 @@ class Airplane {
 */
 
 class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+    this.stomach = [];
+  }
+  eat(someFood) {
+    if (this.stomach.length < 10) {
+      this.stomach.push(someFood);
+    }
+  }
+  poop(){
+    this.stomach.length = 0;
+  }
+  toString(name, age){
+return `${this.name}, ${this.age}`;
+  }
 
 }
 
